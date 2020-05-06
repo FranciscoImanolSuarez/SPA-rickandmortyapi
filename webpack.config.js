7551a537ef49eaba80d2623edff6d46311790d28
Francisco Imanol Suarez <imanolsuarez98@gmail.com>
2020-05-06 18:59:18 -0300

    webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require ('path'); //permite hacia donde estamos dentro del directorio (l;ocal o nube)
+const HtmlWebpackPlugin = require ('html-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output:{
+        path:path.resolve(__dirname, 'dist'),
+        filename: "main.js"
+    },
+    resolve:{
+        extensions: ['.js']
+    },
+    module:{
+        rules: [
+            {
+                test: /\.js?$/, //Reject
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin([{ //template principal
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html',
+        }])
+    ]
+}
