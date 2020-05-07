c3593df0c4fcd7ac127b770ccd9c72a614cc2f52
Francisco Imanol Suarez <imanolsuarez98@gmail.com>
2020-05-07 18:39:07 -0300

    add styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require ('path'); //permite hacia donde estamos dentro del directorio (l;ocal o nube)
 const HtmlWebpackPlugin = require ('html-webpack-plugin');
-
+const CopyWebpackPlugin = require ('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -28,5 +28,9 @@ module.exports = {
             template: './public/index.html',
             filename: './index.html',
         }),
+        new CopyWebpackPlugin([{
+            from: './src/styles/styles.css',
+            to: ''
+        }])
     ]
 }
